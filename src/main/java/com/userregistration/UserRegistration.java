579b68b7b901e51ea39965dd37ca963f3137bbb8
Priyankateke <priiyankateke1705@gmail.com>
2020-04-01 15:01:02 -0700

    ADDED: Test Case And Method For Validate First Name

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,6 +1,12 @@
 package com.userregistration;
 
 public class UserRegistration {
+    String NAME_PATTERN="^[A-Z][a-z]{2,}$";
+
+    public boolean validInputName(String inputName) {
+        return inputName.matches(NAME_PATTERN);
+    }
+
     public static void main(String[] args)
     {
         System.out.println("Welcome To User Registration");
--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -1,6 +1,20 @@
 package com.userregistration;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserRegistrationTest {
    UserRegistration validator=new UserRegistration();
 
+   @Test
+   public void givenFirstName_WhenProper_ShouldReturnTrue() {
+      boolean firstName = validator.validInputName("Priyanka");
+      Assert.assertTrue(firstName);
+   }
+
+   @Test
+   public void givenFirstName_WhenImproper_ShouldReturnFalse() {
+      boolean firstName =validator.validInputName("priyu01");
+      Assert.assertFalse(firstName);
+   }
 }
