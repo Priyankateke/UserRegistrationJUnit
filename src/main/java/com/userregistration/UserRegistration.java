727c9db65fcd36f25fab8708bd94019d937d28db
Priyankateke <priiyankateke1705@gmail.com>
2020-04-01 15:20:37 -0700

    ADDED: Test Case And Method For Validate Email

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -2,9 +2,14 @@ package com.userregistration;
 
 public class UserRegistration {
     String NAME_PATTERN="^[A-Z][a-z]{2,}$";
+    String EMAIL_PATTERN = "^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
 
     public boolean validInputName(String inputName) {
         return inputName.matches(NAME_PATTERN);
+    }
+
+    public boolean validateEmailId(String emailId) {
+        return emailId.matches(EMAIL_PATTERN);
     }
 
     public static void main(String[] args)
--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -29,4 +29,15 @@ public class UserRegistrationTest {
       boolean lastName =validator.validInputName("TEK1");
       Assert.assertFalse(lastName);
    }
+
+   @Test
+   public void givenEmailId_WhenProper_ShouldReturnFalse() {
+      boolean emailId = validator.validateEmailId("abc@gail.com");
+      Assert.assertTrue(emailId);
+   }
+   @Test
+   public void givenEmailId_WhenImproper_ShouldReturnFalse() {
+      boolean emailId = validator.validateEmailId("abcgail.com");
+      Assert.assertFalse(emailId);
+   }
 }
